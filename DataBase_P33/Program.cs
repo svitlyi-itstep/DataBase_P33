1dd50ea74e960e0586e17a51a0f53a033c483f66
svitlyi <svitlyi_a@itstep.academy>
2025-03-06 19:22:19 +0200

    Lesson code

--- DataBase_P33/Program.cs
+++ DataBase_P33/Program.cs
@@ -6,55 +6,7 @@ using System.Text;
 
 class Program
 {
-    public static void CreateTables(MySqlConnection connection)
-    {
-        string query = "CREATE TABLE IF NOT EXISTS Doctors ( " +
-            "ID INT AUTO_INCREMENT PRIMARY KEY," +
-            "Name VARCHAR(100)," +
-            "Premium DECIMAL(15, 2)," +
-            "Salary DECIMAL(15, 2)" +
-            ");";
-        using(MySqlCommand cmd = new MySqlCommand(query, connection))
-        {
-            cmd.ExecuteNonQuery();
-        }
-    }
-
-    public static void AddDoctor(MySqlConnection connection, Doctor doctor)
-    {
-        string query = "INSERT INTO Doctors (Name, Premium, Salary) " +
-            "VALUES (@name, @premium, @salary);";
-        using(MySqlCommand cmd = new MySqlCommand(query, connection)) 
-        {
-            cmd.Parameters.AddWithValue("@name", doctor.Name);
-            cmd.Parameters.AddWithValue("@premium", doctor.Premium);
-            cmd.Parameters.AddWithValue("@salary", doctor.Salary);
-            cmd.ExecuteNonQuery();
-        }
-    }
-
-    public static List<Doctor> GetDoctors(MySqlConnection connection)
-    {
-        string query = "SELECT * FROM Doctors;";
-        List<Doctor> doctors = new List<Doctor>();
-        using(MySqlCommand cmd = new MySqlCommand(query, connection))
-        using(MySqlDataReader reader = cmd.ExecuteReader()) 
-        {
-            while(reader.Read())
-            {
-                doctors.Add(new Doctor(
-                    (int)reader["ID"],
-                    (string)reader["Name"],
-                    (decimal)reader["Premium"],
-                    (decimal)reader["Salary"]
-                ));
-            }
-            
-        }
-        return doctors;
-    }
-
-    public static void ShowDoctors(IEnumerable<Doctor> doctors)
+        public static void ShowDoctors(IEnumerable<Doctor> doctors)
     {
         Console.WriteLine($"| {"ID", 3} | {"Назва", 20} | {"Премія",13} | " +
             $"{"Зарплатня", 14} |");
@@ -81,11 +33,33 @@ class Program
             try
             {
                 connection.Open();
+                DBManager db = new DBManager(connection);
+
                 Console.WriteLine($"Successfully connected to data base {db_database}");
-                CreateTables(connection);
-                AddDoctor(connection, new Doctor("Іванов Іван", 200, 1000));
-                List<Doctor> doctors = GetDoctors(connection);
+                db.CreateTables();
+                List<Doctor> doctors = db.GetDoctors();
                 ShowDoctors(doctors);
+
+                Console.ReadLine();
+                db.AddDoctor(new Doctor("Биков Андрій", 340, 7400));
+                doctors = db.GetDoctors();
+                Doctor last_record = doctors.Last();
+                ShowDoctors(doctors);
+
+                Console.ReadLine();
+                last_record.Name = "Биков Ілля";
+                db.EditDoctor(last_record.ID, last_record);
+                doctors = db.GetDoctors();
+                last_record = doctors.Last();
+                ShowDoctors(doctors);
+
+                Console.ReadLine();
+                db.RemoveDoctor(last_record.ID);
+                doctors = db.GetDoctors();
+                ShowDoctors(doctors);
+
+
+
             }
             catch (Exception ex)
             {
--- DataBase_P33/hospital.cs
+++ DataBase_P33/hospital.cs
@@ -1,6 +1,92 @@
-﻿
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+
 namespace Hospital
 {
+    class DBManager
+    {
+        MySqlConnection connection;
+
+        public DBManager(MySqlConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public void CreateTables()
+        {
+            string query = "CREATE TABLE IF NOT EXISTS Doctors ( " +
+                "ID INT AUTO_INCREMENT PRIMARY KEY," +
+                "Name VARCHAR(100)," +
+                "Premium DECIMAL(15, 2)," +
+                "Salary DECIMAL(15, 2)" +
+                ");";
+            using (MySqlCommand cmd = new MySqlCommand(query, connection))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void AddDoctor(Doctor doctor)
+        {
+            string query = "INSERT INTO Doctors (Name, Premium, Salary) " +
+                "VALUES (@name, @premium, @salary);";
+            using (MySqlCommand cmd = new MySqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@name", doctor.Name);
+                cmd.Parameters.AddWithValue("@premium", doctor.Premium);
+                cmd.Parameters.AddWithValue("@salary", doctor.Salary);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int RemoveDoctor(int id)
+        {
+            string query = "DELETE FROM Doctors WHERE ID=@id;";
+            using (MySqlCommand cmd = new MySqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int EditDoctor(int id, Doctor update)
+        {
+            string query = "UPDATE Doctors " +
+                "SET ID=@id, Name=@name, Premium=@premium, Salary=@salary " +
+                "WHERE ID=@target_id;";
+            using (MySqlCommand cmd = new MySqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@target_id", id);
+                cmd.Parameters.AddWithValue("@id", update.ID);
+                cmd.Parameters.AddWithValue("@name", update.Name);
+                cmd.Parameters.AddWithValue("@premium", update.Premium);
+                cmd.Parameters.AddWithValue("@salary", update.Salary);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public List<Doctor> GetDoctors()
+        {
+            string query = "SELECT * FROM Doctors;";
+            List<Doctor> doctors = new List<Doctor>();
+            using (MySqlCommand cmd = new MySqlCommand(query, connection))
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    doctors.Add(new Doctor(
+                        (int)reader["ID"],
+                        (string)reader["Name"],
+                        (decimal)reader["Premium"],
+                        (decimal)reader["Salary"]
+                    ));
+                }
+
+            }
+            return doctors;
+        }
+    }
+    
     class Doctor
     {
         public int ID { get; set; }
